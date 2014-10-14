eb3bd5ebc2a7a30b1d08e6684ed3530bc3bba7a2
Stephan Bönnemann <stephan@boennemann.me>
2014-10-15 01:53:47 +0200

    feat: barcamp styling and content

--- config.js
+++ config.js
@@ -3,10 +3,11 @@ var config = {
   baseUrl: 'http://localhost:7890',
 
   debug: false,
-  title: 'Twitter wall built by @rem',
+  title: 'Barcamp "nextweb" 2014',
 
-  search: 'from:@fullfrontalconf OR @fullfrontalconf OR #fullfrontalconf OR #fullfrontal2011 OR full-frontal.org OR #fullfrontal11',
-  list: 'fullfrontalconf/delegates11', // optional, just comment it out if you don't want it
+  // search: 'from:@conc_at OR @conc_at OR #concat OR #concat15 OR #concat2015',
+  search: 'from:@salzburgwebdev OR @salzburgwebdev OR #nextweb OR #nextweb14 OR #nextweb2014 OR barcamp-sbg.at',
+  // list: 'fullfrontalconf/delegates11', // optional, just comment it out if you don't want it
 
   timings: {
     showNextScheduleEarlyBy: '5m', // show the next schedule 10 minutes early
